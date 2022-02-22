84fda17f2706b64752963fd3b2cca5d1b0bf989b
Pranav Vashi <neobuddy89@gmail.com>
2022-02-22 10:24:51 +0100

    Ship as crdroid faceunlock service
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

---
+++ app/src/main/java/com/crdroid/faceunlock/camera/listeners/ByteBufferCallbackListener.java
@@ -0,0 +1,4 @@
+package com.crdroid.faceunlock.camera.listeners;
+
+public interface ByteBufferCallbackListener extends CallableEventListener {
+}
---
+++ app/src/main/java/com/crdroid/faceunlock/camera/listeners/CallableEventListener.java
@@ -0,0 +1,5 @@
+package com.crdroid.faceunlock.camera.listeners;
+
+public interface CallableEventListener {
+    void onEventCallback(int i, Object value);
+}
